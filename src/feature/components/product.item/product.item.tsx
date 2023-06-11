e9fac756b251cf1bf712075d569b984126dabd0a
marcos schernetzki <marcoschernetzki@gmail.com>
2023-06-11 19:17:45 +0200

    add product item

---
+++ src/feature/components/product.item/product.item.tsx
@@ -0,0 +1,28 @@
+import { ProductI } from '../../wishes/model/product';
+import { useAppointments } from '../../wishes/hook/use.appointments';
+import './product.item.css';
+
+export const ProductItem = ({ item }: { item: ProductI }) => {
+    const { handleSelect } = useAppointments();
+
+    return (
+        <>
+            <div className="container-service-item">
+                <img
+                    className="service-image"
+                    src={item.image}
+                    alt={item.name}
+                    width="150px"
+                    height="auto"
+                    onClick={(e) => {
+                        e.preventDefault();
+                        handleSelect(item);
+                    }}
+                />
+                <p className="service-name">{item.name}</p>
+                <p className="service-duration">Duracion: {item.duration}</p>
+                <p className="service-price">€ {item.price},00</p>
+            </div>
+        </>
+    );
+};
